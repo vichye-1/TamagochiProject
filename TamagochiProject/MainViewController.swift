d8ddf4f2bc5295ca1afe5f0cfee370650977a91a
knockknock <yangban11111@gmail.com>
2024-06-12 17:11:41 +0900

    code base 기반 layout 구성 준비, asset 추가

--- TamagochiProject/MainViewController.swift
+++ TamagochiProject/MainViewController.swift
@@ -7,13 +7,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = UIColor(red: 245/255, green: 252/255, blue: 252/255, alpha: 1)
     }
-
+    
+    
 
 }
 
